30746b919f408ea152966fdd2f16081ce1ce1c20
Michael Büsch <m@bues.ch>
2026-06-16 21:52:15 +0200

    Add release script

---
+++ scripts/makerelease.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+srcdir="$(dirname "$(realpath "$0")")"
+srcdir="$srcdir/.."
+
+# Import the makerelease.lib
+# https://bues.ch/cgit/misc.git/tree/makerelease.lib
+die() { echo "$*"; exit 1; }
+for path in $(echo "$PATH" | tr ':' ' '); do
+    [ -f "$MAKERELEASE_LIB" ] && break
+    MAKERELEASE_LIB="$path/makerelease.lib"
+done
+[ -f "$MAKERELEASE_LIB" ] && . "$MAKERELEASE_LIB" || die "makerelease.lib not found."
+
+hook_get_version()
+{
+    version="$(cargo_local_pkg_version mailarch)"
+}
+
+project=mailarch
+makerelease "$@"
